45b3293074ff16420a9b2f8f41bbde55450c66f7
ksanchez99 <killersanchez43@gmail.com>
2023-08-07 02:31:34 -0500

    Javascript project 1

---
+++ Basic Javascrip Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,3 @@
+var sent = Sent1 = "This is the beginning of the string", Sent2 = "and this is the end of the string" 
+document.write("This is the beginning of the string " + " and this is the end of the string.");
+document.alert("This is an alert");
