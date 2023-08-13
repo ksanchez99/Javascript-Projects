498b2adc81742d64434e179440c519c5e2625a8e
ksanchez99 <killersanchez43@gmail.com>
2023-08-13 00:14:55 -0500

    project5

---
+++ Basic Javascrip Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,14 @@
+//Boolean Logic
+document.write(10 < 2);
+document.write(55 > 71);
+
+//console log
+console.log(5 - 3);
+
+//Double and triple equals
+document.write(15 == 15);
+document.write(9/11/2001 === 9/11/2001);
+
+//Logical Operators
+document.write(7 < 19 && 10 > 32);
+
