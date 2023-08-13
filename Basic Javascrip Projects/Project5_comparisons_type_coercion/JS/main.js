c4f4f2d9a457e861e9ca20fd51e9eb8a03686da6
ksanchez99 <killersanchez43@gmail.com>
2023-08-13 00:43:09 -0500

    project push

--- Basic Javascrip Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic Javascrip Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -12,3 +12,15 @@ document.write(9/11/2001 === 9/11/2001);
 //Logical Operators
 document.write(7 < 19 && 10 > 32);
 
+//Or operator
+document.write(7 < 11 || 15 < 48);
+
+//Not Function
+function NOT_Function() {
+    document.getElementById("NOT") .innerHTML = !(19 > 88);
+}
+
+// Combination Expression
+var username = 'agent';
+var joined = '47 ' + username;
+document.write(username  + '47')
