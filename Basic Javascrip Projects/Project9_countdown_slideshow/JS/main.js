ee929a957b3f61a3254277b254ce29a3fb45c07d
ksanchez99 <killersanchez43@gmail.com>
2023-08-15 02:35:42 -0500

    Slideshow and timer

---
+++ Basic Javascrip Projects/Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,45 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+//Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+//Thubnail img controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+
+//timer function
+function countdown() {
+    var seconds = document.getElementById("seconds").value;
+
+    function tick() {
+        seconds = seconds - 1;
+        TimeRanges.innerHTML = seconds;
+        var time = setTimeout(tick, 1000);
+        if(seconds == -1) {
+            alert("Times up Mr. Bond!");
+            clearTimeout(time);
+            TimeRanges.innerHTML = "";
+        }
+    }
+    tick();
+}
